6c1b0a61f9c217a4cdf811d0ae894dae24cc2bf2
might be dave ✨ <124959761+adhd@users.noreply.github.com>
2023-03-07 23:25:23 -0500

    Added cool hacker effect.

--- 15-piano-keys/main.js
+++ 15-piano-keys/main.js
@@ -100,3 +100,52 @@ startOver.onclick = function() {
   document.getElementById('word-six').innerHTML = 'YOU!';
   document.getElementById('letter-note-six').innerHTML = 'B';
 }
+
+
+/*
+Hacker Effect
+
+sets up an event listener on the mouseover event of the first h1 element found 
+in the HTML document. When the mouse hovers over the h1 element, the code will 
+start generating random letters, one at a time, and display them in the h1 element
+
+
+*/
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+// on mouse hover, the event listener function will execute
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0; // keep track of the number of letters that have been generated so far
+  
+  clearInterval(interval); // clears interval timer by calling clearInterval() on "interval" variable
+  
+  interval = setInterval(() => { // interval timer using setInterval()
+    /*
+    generates a new string of letters by looping through each character of the original string and 
+    replacing it with either a random letter or the original character... loop continues until all 
+    characters have been replaced by their original letters
+    */
+    event.target.innerText = event.target.innerText
+      .split("") // splits the text content of the h1 element into an array of individual characters 
+      .map((letter, index) => { // loop through each char & replace w/ either original char or a randomly generated letter
+        if(index < iteration) {
+          // if (index of curr char) < ("iteration" variable), return original char at that position
+          return event.target.dataset.value[index];
+        }
+        // otherwise return random letter
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    // once all chars replaced by original letters, the interval timer is cleared
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    // "iteration" variable incremented by 1/3 to continue loop & generate another letter until 
+    // all characters in the original text have been replaced
+    iteration += 1 / 3;
+  }, 30); // timer will execute the given function every 30 milliseconds until it is cleared
+}
